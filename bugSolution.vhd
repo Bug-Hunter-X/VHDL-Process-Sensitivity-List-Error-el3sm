5c672541c14c089432f1622a7ed7a3953e19d4af
BugHunter-X <askzenai@gmail.com>
2025-01-31 07:36:06 +0100

    Add bugSolution.vhd file

---
+++ bugSolution.vhd
@@ -0,0 +1,17 @@
+```vhdl
+ENTITY good_process IS
+  PORT (clk : IN STD_LOGIC;
+        data_in : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        data_out : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+END ENTITY good_process;
+
+ARCHITECTURE behavioral OF good_process IS
+BEGIN
+  PROCESS (clk, data_in) -- Added data_in to the sensitivity list
+  BEGIN
+    IF rising_edge(clk) THEN
+      data_out <= data_in;
+    END IF;
+  END PROCESS;
+END ARCHITECTURE;
+```
